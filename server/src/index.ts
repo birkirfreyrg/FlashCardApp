11e0572ed91e6e9403bf02e712b3a1b657079451
birkirfreyrg <birkirfrey@msn.com>
2023-01-25 03:08:33 +0000

    Adding the ability to delete the decks, and refactoring code for some good looking code.

--- server/src/index.ts
+++ server/src/index.ts
@@ -34,6 +34,16 @@ app.post("/decks", async (req: Request, res: Response) => {
     res.json(createdDeck);
 });
 
+app.delete('/decks/:deckId', async (req: Request, res: Response) => {
+    // TODO:
+    // 1. get the deck id from the url
+    const deckId = req.params.deckId;
+    // 2. delete the deck from mongo
+    const deck = await Deck.findByIdAndDelete(deckId);
+    // 3. return the deleted deck to the user who made the request
+    res.json(deck);
+});
+
 mongoose.connect(process.env.MONGO_URL!).then(() => {
 console.log(`Listening on port ${PORT}`);
     app.listen(PORT);
